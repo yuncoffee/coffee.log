f8663baea9208b270559e20b251d6b7f219345d1
coffeegom <dong072815@gmail.com>
2021-10-31 16:22:47 +0900

    [style] right section 추가

---
+++ src/js/input.js
@@ -0,0 +1,51 @@
+;`<div class="container">
+<div class="container-selectBox">
+    <div class="selectBox">
+        <div class="input-selectBox">g</div>
+    </div>
+    <div class="optionBox">
+        <ul class="optionList">
+            <li class="optionItem-0" value="">option1</li>
+            <li class="optionItem-1" value="">option2</li>
+            <li class="optionItem-2" value="">option3</li>
+            <li class="optionItem-3" value="">option4</li>
+        </ul>
+    </div>
+</div>
+<div class="container-selectBox">
+    <div class="selectBox">
+        <div class="input-selectBox">g</div>
+    </div>
+    <div class="optionBox">
+        <ul class="optionList">
+            <li class="optionItem-0" value="">option1</li>
+            <li class="optionItem-1" value="">option2</li>
+            <li class="optionItem-2" value="">option3</li>
+            <li class="optionItem-3" value="">option4</li>
+        </ul>
+    </div>
+</div>
+</div>
+<div class="container-selectBox">
+<div class="selectBox">
+    <div class="input-selectBox select-card">g</div>
+</div>
+<div class="optionBox">
+    <ul class="optionList">
+        <li class="optionItem-0" value="">option1</li>
+        <li class="optionItem-1" value="">option2</li>
+        <li class="optionItem-2" value="">option3</li>
+        <li class="optionItem-3" value="">option4</li>
+    </ul>
+</div>
+</div>
+<div class="container-radioBox">
+<input name="radio-0" type="radio" id="radioItem-0" />
+<label for="radioItem-0">radio1</label>
+
+<input name="radio-0" type="radio" id="radioItem-1" />
+<label for="radioItem-1">radio2</label>
+
+<input name="radio-0" type="radio" id="radioItem-2" />
+<label for="radioItem-2">radio3</label>
+</div>`
